ecdb64be0c235f44eafa50999dd85b7cd18b3a19
Dominika Matczynska <dominika.matczynska@stxnext.pl>
2023-08-12 15:17:40 +0200

    Refactor js code, add styling

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-class BookRoulette {
-    constructor(searchButton) {
-        this.searchButton = searchButton;
-        this.searchButton.addEventListener('click', this.getRandBookInfo);
+async function getSubjectCollection() {
+    const selectedType = document.querySelector('input[name="subject"]:checked').value;
+    const offset = Math.floor((Math.random() * 500));
+    
+    const response = await axios.get(`https://openlibrary.org/subjects/${selectedType}.json?limit=10&offset=${offset}`)
+    getRandomBook(response.data.works)
+}
+
+async function getRandomBook(books) {    
+    const index = Math.floor((Math.random() * 10));
+    const bookKey = books[index].key;
+    const response = await axios.get(`https://openlibrary.org${bookKey}.json`);
+
+    const bookTitle = response.data.title;
+    const authorName = books[index].authors[0].name;
+    const coverId = books[index].cover_id;
+    const coverImage = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`
+    let bookDescription;
+
+    if (!response.data.description) {
+        bookDescription = "No description available";
+    }
+    else if (response.data.description.value) {
+        bookDescription = response.data.description.value.replace(/\*/g, '');
+    }
+    else {
+        bookDescription = response.data.description.replace(/\*/g, '');
     }
 
-    getRandBookInfo = () => {
-        const selectedType = document.querySelector('input[name="subject"]:checked').value;
-        const offset = Math.floor((Math.random() * 200));
+    renderBookInfo(bookTitle, authorName, bookDescription, coverImage)
+}
 
-        axios.get(`https://openlibrary.org/subjects/${selectedType}.json?limit=10&offset=${offset}`)
-        .then((response) => {
-            const books = response.data.works;
-            const idx = Math.floor((Math.random() * 10));
-            const bookKey = books[idx].key;
-            this.cover_id = books[idx].cover_id
-            this.author = books[idx].authors.map((author) => ` ${author.name}`)
-            return axios.get(`https://openlibrary.org${bookKey}.json`);
-        })
-        .then((response) => {
-            const title = document.querySelector('#title');
-            const author = document.querySelector('#author');
-            const description = document.querySelector('#description');
-            const cover = document.querySelector('#cover');
-            cover.src = `https://covers.openlibrary.org/b/id/${this.cover_id}-L.jpg`
+function renderBookInfo(title, author, description, cover) {
+    const bookSummary = document.querySelector('#book-summary')
 
-            title.innerHTML = `"${response.data.title}"`;
-            author.innerHTML = `By ${this.author}`;
-
-            if (!response.data.description) {
-                description.innerHTML = "Sorry, no description available!";
-            }
-            else if (response.data.description.value) {
-                description.innerHTML = response.data.description.value;
-            }
-            else {
-                description.innerHTML = response.data.description;
-            }
-        })
-    } 
-    }
+    bookSummary.innerHTML = `
+      <div id="content">
+          <h2>${title}</h2>
+          <h3>${author}</h3>
+          <p>${description}</p>
+          <img src="${cover}">
+      </div>
+    `
+}
 
 const searchButton = document.querySelector('#search');
-const bookRoulette = new BookRoulette(searchButton)
+searchButton.addEventListener('click', getSubjectCollection);
